5009cb29354b1acbd8b28cdfdd589e5a45e77ca3
Sukhmandeep Singh <sukhmandeep@myyahoo.com>
2023-03-02 13:36:48 -0800

    Dummy Commit to check deployment

--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -1,9 +1,16 @@
 import TopNavbar from "../components/TopNavbar";
+import { Container } from "react-bootstrap";
 
 const TitlePage = () => {
 	return (
 		<div>
 			<TopNavbar />
+			<Container>
+				<h1>Pitchi Paaji</h1>
+				<h1>Kaata Paau</h1>
+				<h1>Nunny Baba</h1>
+				<h1>Puli</h1>
+			</Container>
 		</div>
 	);
 };
